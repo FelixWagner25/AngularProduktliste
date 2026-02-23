23cff7dec972cad357f6122ddaaba27e9c44ebef
Felix Wagner <post@felixwagner.eu>
2026-02-23 15:52:18 +0100

    use CurrencyPipe for currency display

--- src/app/shared/components/product-list/product-list.component.ts
+++ src/app/shared/components/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ProductService } from '../../services/product.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [RouterLink],
+  imports: [RouterLink, CurrencyPipe],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
