b9dba1085924e8e7c9528ea69b38c9be50042f62
Felix Wagner <post@felixwagner.eu>
2026-02-23 12:10:46 +0100

    Add no found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProductDetailComponent } from './shared/components/product-detail/product-detail.component';
 import { ProductListComponent } from './shared/components/product-list/product-list.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path: "", component: ProductListComponent},
-    {path: "detail/:name", component: ProductDetailComponent}
+    {path: "detail/:name", component: ProductDetailComponent},
+    {path: "not-found", component: PageNotFoundComponent},
+    {path:"**", redirectTo: "not-found"}
+
 ];
